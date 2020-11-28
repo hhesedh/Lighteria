4d438482bd7480e0308d8d2f98a4bfc5d0bcf35b
Fábio Oliveira <fabiodeoliveira07@gmail.com>
2020-11-28 01:24:34 -0300

    Adição de imagens, fotos e cabecalho

--- App.tsx
+++ App.tsx
@@ -1,21 +1,56 @@
-import { StatusBar } from "expo-status-bar";
+import { AppLoading } from "expo";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, SafeAreaView, View, Image, StyleSheet } from "react-native";
+import * as Font from "expo-font";
 
-export default function App() {
+const customFonts = {
+  "OpenSans-ExtraBold": require("./assets/fonts/OpenSans-ExtraBold.ttf"),
+};
+
+const App = () => {
+  const [loaded] = Font.useFonts(customFonts);
+
+  if (!loaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.containerTitulo}>
+        <Text style={styles.titulo}>Lighteria</Text>
+        <View style={styles.containerSacola}>
+          <Image
+            source={require("./assets/images/icone-sacola.png")}
+            style={styles.imagem}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
+  container: { flex: 1, backgroundColor: "#f4f0f4" },
+  containerTitulo: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    paddingHorizontal: 24,
+  },
+
+  titulo: {
+    fontFamily: "OpenSans-ExtraBold",
+    fontSize: 28,
+  },
+  containerSacola: {
+    backgroundColor: "#fff",
+    padding: 18,
+    borderRadius: 30,
+  },
+  imagem: {
+    height: 30,
+    width: 30,
   },
 });
+
+export default App;
